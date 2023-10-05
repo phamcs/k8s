c4c5cf6c79606f691769383c905b7b72abc817d8
Pham Computer Science <phamcs@users.noreply.github.com>
2023-10-05 08:46:58 -0600

    Update k8s-bootstrap.sh

--- k8s-bootstrap.sh
+++ k8s-bootstrap.sh
@@ -51,6 +51,6 @@ mkdir -p /var/log/kubernetes/audit
 
 ## Install NFS Client Drivers and HELM
 apt-get update && apt-get install -y nfs-common
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 apt-get update && apt-get install -y helm
